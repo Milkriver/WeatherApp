b08c530c82c25dafb8601f3661d0d256fc0b8a3b
Milkriver <mapav4@yandex.ru>
2022-02-25 20:55:44 +0500

    Эксперименты

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const daysOfWeekMap = {
-    0: 'SUN', 
-    1: 'MON', 
-    2: 'TUES', 
-    3: 'WED', 
-    4: 'THUR', 
-    5: 'FRI', 
+    0: 'SUN',
+    1: 'MON',
+    2: 'TUES',
+    3: 'WED',
+    4: 'THUR',
+    5: 'FRI',
     6: 'SAT'
 }
 
 const iconNameToSizeMap = {
-    cloudy: { width: 264, height: 166},
-    sunny: { width: 208, height: 213},
-    stormy: { width: 246, height: 187},
-    snowy: { width: 230, height: 196},
-    'partly-cloudy': {width: 230, height:209},
-    rainy: { width: 160, height: 222},
+    cloudy: { width: 264, height: 166 },
+    sunny: { width: 208, height: 213 },
+    stormy: { width: 246, height: 187 },
+    snowy: { width: 230, height: 196 },
+    'partly-cloudy': { width: 230, height: 209 },
+    rainy: { width: 160, height: 222 },
 }
 
 const today = new Date();
@@ -24,9 +24,16 @@ const year = today.getFullYear();
 const month = today.getMonth();
 let weekDay = daysOfWeekMap[day];
 
-let days = document.getElementsByClassName('.day-of-week');
+let days = document.querySelectorAll('.day-of-week')[0];
 let weather = document.getElementsByClassName('.weather');
 let temperature = document.getElementsByClassName('.temperature');
 for (let key in daysOfWeekMap) {
-    console.log( daysOfWeekMap[key] ); 
-  }
+    // console.log(daysOfWeekMap[key]);
+    // days.innerHTML = value;
+
+}
+let a = Array.from(document.querySelectorAll('.day-of-week'));
+for (let index = 0; index < a.length; index++) {
+    console.log(a[index].innerHTML=daysOfWeekMap[day]);  
+}
+
